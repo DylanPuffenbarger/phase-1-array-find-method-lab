f4703f2dab6b1ee29bbac4e2816150daf7732829
Dylan Puffenbarger <dylanpuffenbarger@gmail.com>
2023-08-20 11:33:31 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    const cb = (element) => element.result === 'W';
+    const win = record.find(cb);
+    if (win === undefined){
+        return undefined;
+    } else {
+        return win.year;
+    }
+}
